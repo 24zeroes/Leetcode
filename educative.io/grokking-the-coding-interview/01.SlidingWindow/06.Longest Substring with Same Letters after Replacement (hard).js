5d42343608ae3a2cb763114106ec91b54aec16b3
24zeroes <vdmbat@gmail.com>
2021-07-02 20:33:42 +0300

    length_of_longest_substring

--- educative.io/grokking-the-coding-interview/01.SlidingWindow/06.Longest Substring with Same Letters after Replacement (hard).js
+++ educative.io/grokking-the-coding-interview/01.SlidingWindow/06.Longest Substring with Same Letters after Replacement (hard).js
@@ -1,35 +1,37 @@
 const length_of_longest_substring = function(str, k) {
-    var l = 0;
-    var r = 0;
-    var lastCharacter = str[l];
-    var tempK = k;
-    var result = -Infinity;
-  
-    while (r < str.length)
+  var l = 0;
+  var r = 0;
+  var tempK = k;
+  var letter = str[l];
+  var result = -Infinity;
+
+  while (r < str.length)
+  {
+    console.log(l + " " + r );
+    if (str[r] == letter)
     {
-      if (lastCharacter == str[r])
+      result = Math.max(result, r - l + 1);
+      r += 1;
+    }
+    else
+    {
+      if (tempK > 0)
       {
         result = Math.max(result, r - l + 1);
+        tempK -= 1;
         r += 1;
       }
       else
       {
-        if (tempK > 0)
+        while(str[l] == letter)
         {
-          result = Math.max(result, r - l + 1);
-          tempK -= 1;
-          r += 1;
+          l += 1;
         }
-        else
-        {
-          l = r;
-          r = r + 1;
-          tempK = k;
-          lastCharacter = str[l];
-        }
+        r = l;
+        tempK = k;
+        letter = str[r];
       }
     }
-  
-    return result;
-  };
-  
+  }
+  return result;
+};
