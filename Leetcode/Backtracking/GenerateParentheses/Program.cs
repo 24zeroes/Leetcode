b3013e04d8ee422c5b34270f01c0609a85c74d07
24zeroes <vdmbat@gmail.com>
2021-11-07 18:37:23 +0300

    GenerateParentheses

---
+++ Leetcode/Backtracking/GenerateParentheses/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenerateParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var result = solution.Solve(3);
+            Console.WriteLine("End");
+        }
+    }
+
+    class Solution
+    {
+        private List<string> Result { get; set; }
+        private int N { get; set; }
+        public List<string> Solve(int n)
+        {
+            Result = new List<string>();
+            N = n;
+            SolveInternal(0, 0, "");
+            return Result;
+        }
+
+        public void SolveInternal(int open, int close, string cur)
+        {
+            if (open + close == 2 * N)
+            {
+                Result.Add(cur);
+            }
+
+            if (open < N)
+            {
+                SolveInternal(open + 1, close, cur + "(");
+            }
+
+            if (close < open)
+            {
+                SolveInternal(open, close + 1, cur + ")");
+            }
+        }
+    }
+}
