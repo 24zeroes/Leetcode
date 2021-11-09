c6ae43d15291abe66d6b07e06a06b5b472d75eca
24zeroes <vdmbat@gmail.com>
2021-11-09 20:08:12 +0300

    SumOfTwoIntegers

---
+++ Leetcode/SumOfTwoIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SumOfTwoIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            s.Solve(2, 3);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Solution
+    {
+        public int Solve(int a, int b)
+        {
+            while (b != 0)
+            {
+                int carry = a & b;
+                a = a ^ b;
+                b = carry << 1;
+            }
+            return a;
+        }
+    }
+}
