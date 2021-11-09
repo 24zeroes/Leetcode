aa06e8ee8cab9ac5e723c6f47561e24439aa1f88
24zeroes <vdmbat@gmail.com>
2021-11-09 21:28:20 +0300

    JumpGame

---
+++ Leetcode/JumpGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace JumpGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.Solve(new[] {3, 2, 1, 1, 4});
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Solution
+    {
+        public bool Solve(int[] inp)
+        {
+            var lastGoodPosition = inp.Length - 1;
+            for (var i = inp.Length - 1; i >= 0; i--)
+            {
+                if (i + inp[i] >= lastGoodPosition)
+                {
+                    lastGoodPosition = i;
+                }
+            }
+
+            return lastGoodPosition == 0;
+        }
+    }
+}
